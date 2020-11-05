0c2f443ad2fb3cf702bbc9244fcaf7d122c29e6e
Andrew <andrewras93@gmail.com>
2020-11-05 10:54:04 +0100

    Forced commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+
+    /* Logo Slider */
+
     $('.customer-logos').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -19,4 +37,5 @@ $(document).ready(function() {
             }
         }]
     });
+
 });
